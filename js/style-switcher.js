4ffc1d0a3b57eb2576de21f07e6788e12022a892
Bhaskarrd10 <45232937+Bhaskarrd10@users.noreply.github.com>
2022-05-06 12:51:30 +0530

    Add files via upload

---
+++ js/style-switcher.js
@@ -0,0 +1,14 @@
+
+
+/*styleSwitcherToggler.addEventListener("click", () =>{
+  document.querySelector(".style-switcher").classList.toggle("open");
+
+})
+
+
+window.addEventListener("scroll", () =>{
+  if(document.querySelector(".style-switcher").classList.contains("open")){
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+})
+*/
